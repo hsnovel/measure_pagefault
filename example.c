b80629e4a80e157b04c1f67bc6a9cd48fba436b8
hsnovel <root@hsnovel.net>
2024-07-14 22:20:23 +0200

    update example

--- example.c
+++ example.c
@@ -6,11 +6,11 @@
 
 int main()
 {
-	struct page_fault_ctx ctx = start_pagefault_counter();
-
 	size_t size = 4096 * 32 * 4;
 	int *data = mmap(0, size, PROT_NONE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
 	mprotect(data, size, PROT_READ|PROT_WRITE);
+
+	struct page_fault_ctx ctx = start_pagefault_counter();
 
 	for (int i = 0; i < size / sizeof(int); i++)
 	{
